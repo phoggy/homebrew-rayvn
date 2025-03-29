10eeb3e4f83ba9e119c2334c11cc15df03f5c675
phynix <phynix@phoggy.me>
2025-03-28 21:35:17 -0700

    updsate version and hash

--- Formula/rayvn.rb
+++ Formula/rayvn.rb
@@ -4,7 +4,7 @@ class Rayvn < Formula
     desc "A simple bash shared library system, enabling use of \"require 'project/library'\" to load libraries."
     homepage "https://github.com/phoggy/rayvn"
     version "0.1.2"
-    url "https://github.com/phoggy/rayvn/archive/refs/tags/v0.1.0.tar.gz"
+    url "https://github.com/phoggy/rayvn/archive/refs/tags/v0.1.2.tar.gz"
     sha256 "9ec40ae2d5b2c804ccbcc26b58f72c36f3c7fc44e994b74fab107ae95e615732"
     license "GPL-3.0"
 
